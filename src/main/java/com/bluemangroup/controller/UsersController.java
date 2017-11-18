55914d5d57640cabbf34d0fb1b82782585b7e7e4
tonycapone <anthonyrhowell@gmail.com>
2017-11-18 11:08:10 -0600

    added CrossOrigin

--- src/main/java/com/bluemangroup/controller/UsersController.java
+++ src/main/java/com/bluemangroup/controller/UsersController.java
@@ -5,6 +5,7 @@ import com.bluemangroup.model.User;
 import com.bluemangroup.model.Violations;
 import com.bluemangroup.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController("/users")
+@CrossOrigin
 public class UsersController {
 
     @Autowired
