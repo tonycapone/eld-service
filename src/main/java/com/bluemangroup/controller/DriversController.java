0697d9ce07b4a94b2bd0d2abdb27c8aab0c1663a
tonycapone <anthonyrhowell@gmail.com>
2017-11-18 19:16:18 -0600

    CORS light

--- src/main/java/com/bluemangroup/controller/DriversController.java
+++ src/main/java/com/bluemangroup/controller/DriversController.java
@@ -3,13 +3,11 @@ package com.bluemangroup.controller;
 import com.bluemangroup.model.Location;
 import com.bluemangroup.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@CrossOrigin
 @RestController
 public class DriversController {
     @Autowired
